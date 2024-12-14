8f277453d892c1d48402e524a219e74c9927206b
Lorenzo Verheecke <lorenzo@realo.com>
2024-12-14 10:48:48 +0100

    importeer alle models & de db instantie om alle tables aan te kunnen maken

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from .models import db
 from .config import Config
 from flask import Blueprint
-
-
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)
+    # importeer modellen & database instantie
+    from .models import db, Category, Listing, Notification, Review, Student, Technician, User
 
+    db.init_app(app)
     with app.app_context():
         db.create_all()  # Create sql tables for our data models
 
