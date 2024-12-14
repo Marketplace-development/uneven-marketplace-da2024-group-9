c824fb1e3af14be6edc359fe0385643ea1332379
Lorenzo Verheecke <lorenzo@realo.com>
2024-12-14 10:48:03 +0100

    config om te verbinden met supabase

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
-print("config.py is geladen")
-from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
 
-# Supabase configuratie
-SUPABASE_URL = "https://aws-0-eu-central-1.pooler.supabase.com"
-SUPABASE_KEY = "postgres.qnuzlfxmugobmzrehbwa:Bfg6WSGodQHfazaF"
+load_dotenv()
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-print("Supabase-client is aangemaakt")
-
+class Config:
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get('SUPABASE_URL')
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
