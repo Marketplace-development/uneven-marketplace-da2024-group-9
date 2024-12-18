56013d8e331e53b51f2bb94de9d7c88765767a74
guillaumekeirsebilck <guillaume.keirsebilck@ugent.be>
2024-12-18 14:04:40 +0100

    task detail

--- app/models/Listing.py
+++ app/models/Listing.py
@@ -14,8 +14,7 @@ class Listing(db.Model):
     mime_type = db.Column(db.String(100), nullable=True)
     price = db.Column(db.Float, nullable=True)  
     username = db.Column(db.String(80), nullable=False)  
-
-
+    
     created_at = db.Column(db.DateTime, default=db.func.now())
     transactions = db.relationship('Transaction', backref='parent_listing', lazy=True)
 
